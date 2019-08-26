20113bbb8f1f5dd6e95688b51445388fda9a879a
esterlilianlb <esterlilian2231@gmail.com>
2019-08-26 15:26:19 -0300

    cifragem e decifragem de letras minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,35 @@
 window.cipher = {
-    encode: encode,
-    decode: decode, 
+  encode: encode,
+  decode: decode, 
 };
 
 function encode (textCode, codeOffset) {
   let result="";  
-  for (let i = 0; i < textCode.length; i++) {
-    let message= textCode[i].charCodeAt();
-    let calculo= ((message - 65 + codeOffset) % 26) + 65
-      result += String.fromCharCode(calculo);
-      
-  }
-  return result
+    for (let i = 0; i < textCode.length; i++) {
+      let message= textCode[i].charCodeAt();
+      let calculo= ((message - 65 + codeOffset) % 26) + 65;
+      if (message >=65 && message<=90) {
+        result += String.fromCharCode(calculo);
+      } else if (message >=97 && message <=122) {
+        let calculo= ((message - 97 + codeOffset) % 26) + 97;
+        result += String.fromCharCode(calculo);
+      }
+    } 
+  return result; 
 }
 
 function decode (textDecode, decodeOffset) {
   let result="";
-  for (let i = 0; i < textDecode.length; i++) {
+   for (let i = 0; i < textDecode.length; i++) {
      let message= textDecode[i].charCodeAt();
-     let calculo= ((message - 90 - decodeOffset) % 26) + 90
-      result += String.fromCharCode(calculo);
-  }
-  return result
+     let calculo= ((message - 90 - decodeOffset) % 26) + 90;
+     if (message >=65 && message <=90) {
+       result += String.fromCharCode(calculo);
+     } else if (message >=97 && message <= 122) {
+       let calculo= ((message - 122 - decodeOffset) % 26) + 122;
+       result += String.fromCharCode(calculo);
+     } 
+    }
+  return result;
 }
 
