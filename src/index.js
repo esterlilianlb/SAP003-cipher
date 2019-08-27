0e8ceb75052f14cfb3d91277fd9573205dacf2cf
esterlilianlb <esterlilian2231@gmail.com>
2019-08-27 13:55:29 -0300

    os botões de cifrar e decifrar passam a ser chamados no index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 function encode1() {
-   let textCode= document.getElementById("textcode").value;
-   let codeOffset= Number(document.getElementById("codeoffset").value);
-   let result = cipher.encode(textCode, codeOffset);
-   document.getElementById("result").innerHTML=`${result}`;
+  let textCode= document.getElementById("textcode").value;
+  let codeOffset= Number(document.getElementById("codeoffset").value);
+  let result = cipher.encode(textCode, codeOffset);
+  document.getElementById("result").innerHTML=`${result}`;
 
-   if (codeOffset<0) {
-       return document.getElementById("result").innerHTML=`Insira um núemro maior que 0`;
-   }
+  if (codeOffset<0) {
+    return document.getElementById("result").innerHTML="Insira um núemro maior que 0";
+  }
 }
 
 function decode1() {
-    let textDecode= document.getElementById("textdecode").value;
-    let decodeOffset= Number(document.getElementById("decodeoffset").value);
-    let result = cipher.decode (textDecode, decodeOffset);
-    document.getElementById("result").innerHTML=`${result}`;
+  let textDecode= document.getElementById("textdecode").value;
+  let decodeOffset= Number(document.getElementById("decodeoffset").value);
+  let result = cipher.decode (textDecode, decodeOffset);
+  document.getElementById("result").innerHTML=`${result}`;
 
-    if (decodeOffset<0) {
-        return document.getElementById("result").innerHTML=`Insira um núemro maior que 0`;
-    }
+  if (decodeOffset<0) {
+    return document.getElementById("result").innerHTML="Insira um núemro maior que 0";
+  }
 }
 
